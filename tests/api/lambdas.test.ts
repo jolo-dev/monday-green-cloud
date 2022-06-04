7f3c0d6c67ff88f9466becced3d72da0d9a8d537
John Nguyen <john.nguyen@kreuzwerker.de>
2022-06-04 17:59:51 +0200

    Add Graphql with SST

---
+++ tests/api/lambdas.test.ts
@@ -0,0 +1,23 @@
+import { describe, it, expect, afterEach, vi } from "vitest";
+import { getLambdas } from '../../api/src/lambdas';
+
+vi.mock('../../api/src/lambdas', () => {
+  const dummyLambda = require('./lambda.json')
+  return {
+    getLambdas: vi.fn().mockResolvedValue(dummyLambda)
+  }
+})
+
+describe("api/src/lambdas", () => {
+  
+  
+  afterEach(() => {
+    vi.restoreAllMocks()
+  })
+  
+  it("should get all Lambdas", async () => {
+    const lambdas = await getLambdas();
+    const dummyLambda = require('./lambda.json')
+    expect(lambdas).toEqual(dummyLambda);
+  });
+});
